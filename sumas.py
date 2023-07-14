91289616c24fad76d373abba968e009c04032075
Edwin <q.edwin.ruiz@gmail.com>
2023-07-14 08:40:25 -0700

    new item added

--- sumas.py
+++ sumas.py
@@ -1,5 +1,5 @@
 compras = [{'item':'celular', 'precio':3000},{'item':'tv', 
-'precio':75000}]
+'precio':75000},{'item':'computadora','precio':4500}]
 suma=0
 for items in compras:
     print (items['precio'])
